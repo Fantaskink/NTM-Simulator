b1528d1c07e3abf2d4d0fe51b4b15283f823f2e2
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2024-02-09 09:12:09 +0100

    Add toString for tapeSymbol

--- src/tapeSymbol.java
+++ src/tapeSymbol.java
@@ -1,3 +1,15 @@
 public enum tapeSymbol {
     BLANK, ZERO, ONE, X, X_HAT,
+    ;
+
+    @Override
+    public String toString() {
+        return switch (this) {
+            case BLANK -> "_";
+            case ZERO -> "0";
+            case ONE -> "1";
+            case X -> "x";
+            case X_HAT -> "x̂";
+        };
+    }
 }
